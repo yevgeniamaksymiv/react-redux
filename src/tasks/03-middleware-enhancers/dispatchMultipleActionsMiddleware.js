0963f3dc3b0efb10d279ae5ba5caeefc95ac76be
yevgeniamaksymiv <evgeniamaksymiv@gmail.com>
2022-01-10 11:43:20 +0200

    created counterEvenOddEnhancer dispatchMultipleActionsMiddleware

--- src/tasks/03-middleware-enhancers/dispatchMultipleActionsMiddleware.js
+++ src/tasks/03-middleware-enhancers/dispatchMultipleActionsMiddleware.js
@@ -1,11 +1,4 @@
-
-
 const dispatchMultipleActionsMiddleware = storeAPI => next => action => {
-    // TODO This middleware should check to see if a given "action" is actually an array,
-    // TODO using `Array.isArray()`.  If it's an array, it should assume that all of the
-    // TODO items are real actions, and dispatch them back to the start of the middleware chain.
-    // TODO In that case, the return value should be the number of actions in the array.
-    // Remember that `storeAPI` has the `dispatch` method available.
 
     if (!Array.isArray(action)) return next(action);
     return action.map((a) => storeAPI.dispatch(a)).length;
