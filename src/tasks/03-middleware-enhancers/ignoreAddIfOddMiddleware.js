9847ea5a023ebb05aec1b8e487e18650d1db4ff4
V-Klimonov <822775v@gmail.com>
2022-01-10 11:23:00 +0200

    add ignore condition

--- src/tasks/03-middleware-enhancers/ignoreAddIfOddMiddleware.js
+++ src/tasks/03-middleware-enhancers/ignoreAddIfOddMiddleware.js
@@ -1,14 +1,16 @@
 
 
 const ignoreAddIfOddMiddleware = storeAPI => next => action => {
-    // TODO This middleware should look for "ADD_5" actions, and when it sees one, check
-    // TODO the current state.  If `state.counter` is odd, the middleware should ignore
-    // TODO the action entirely, and prevent it from going onwards.
+  // TODO This middleware should look for "ADD_5" actions, and when it sees one, check
+  // TODO the current state.  If `state.counter` is odd, the middleware should ignore
+  // TODO the action entirely, and prevent it from going onwards.
 
-    // Remember that `storeAPI` has the `getState` method available.
-
-    return next(action);
-
+  // Remember that `storeAPI` has the `getState` method available.
+    if (action.type === "ADD_5" && storeAPI.getState().counter % 2 === 0) {
+        console.log(storeAPI.getState().counter);
+        return next(action);
+    } else if (action.type === "INCREMENT") return next(action);
+    
 }
 
 export default ignoreAddIfOddMiddleware;
