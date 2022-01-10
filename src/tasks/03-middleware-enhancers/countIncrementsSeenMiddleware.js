d4a7662df2a38f681a78c9a2a6a34cc5289918aa
V-Klimonov <822775v@gmail.com>
2022-01-10 14:37:15 +0200

    fix

--- src/tasks/03-middleware-enhancers/countIncrementsSeenMiddleware.js
+++ src/tasks/03-middleware-enhancers/countIncrementsSeenMiddleware.js
@@ -6,15 +6,19 @@
 
 // TODO This means you'll need to keep a variable alive in the middleware between dispatches, so start
 // TODO by splitting up the triple-functions structure so you have a place to put the counter.
-let counter = 0
+let counterIncrementsSeen = 0;
 const countIncrementsSeenMiddleware = storeAPI => next => action => {
     
     if (action.type === "INCREMENT"){
         next(action)
-        counter++
-        return storeAPI.dispatch({ type: "INCREMENTS_SEEN", count: counter });
+        counterIncrementsSeen++;
+        return storeAPI.dispatch({
+          type: "INCREMENTS_SEEN",
+          count: counterIncrementsSeen,
+        });
        
     } 
+    
     return next(action)
 
 }
